b5a745547338c058541b02c1cfdbddba38d75c4d
Aron <aronkerk8@gmail.com>
2023-05-18 16:39:44 -0300

    Adding project view

--- src/Routes.js
+++ src/Routes.js
@@ -2,6 +2,7 @@ import React from "react";
 import Login from "./pages/login";
 import Main from "./pages/main";
 import Register from "./pages/register";
+import Projeto from "./pages/projeto";
 import { Route, Routes, BrowserRouter, Navigate } from "react-router-dom";
 import { UserAuth } from "./context/authcontext";
 import { ProtectedRoute } from "./components/ProtectRoute";
@@ -16,6 +17,11 @@ const Rotas = () => {
                 <Route path="/index" element={
                     <ProtectedRoute user={user}>
                         <Main />
+                    </ProtectedRoute>
+                } />
+                <Route path="/projeto" element={
+                    <ProtectedRoute user={user}>
+                        <Projeto />
                     </ProtectedRoute>
                 } />
                 <Route path="/register" element={user ? <Navigate to={"/index"} replace/> : <Register />} />
